2ce233ff4c24285691abfa2d16d595ea9e98a339
coolgis <mitooo@outlook.com>
2024-09-16 18:19:58 +0800

    feat: 路由懒加载

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
-export function Error() {
+export function NotFound() {
   return (
     <div className="flex justify-center items-center h-[50vh] font-bold text-3xl text-red-500">
-      出错了！
+      页面不存在！
     </div>
   )
 }
