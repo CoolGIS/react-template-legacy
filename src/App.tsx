161b3a60da4ce90682626147e58404187d6a3295
coolgis <mitooo@outlook.com>
2024-09-17 01:12:46 +0800

    refactor: url修改为字符串形式

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,10 @@ function App() {
         <Counter />
         <ul className="flex gap-2.5 justify-center items-center p-2.5 m-2.5 border border-gray-300 rounded-md bg-white">
           <li>
-            <Link to={'/home'}>主页</Link>
+            <Link to="/home">主页</Link>
           </li>
           <li>
-            <Link to={'/about'}>关于</Link>
+            <Link to="/about">关于</Link>
           </li>
         </ul>
         <div>
