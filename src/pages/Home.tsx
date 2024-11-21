05c484b9d370c1d5104448775d87a01e6dc4a378
haoj <haoj@geoscene.com.cn>
2024-11-21 13:16:18 +0800

    feat: 引入 react-use 工具库

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,16 @@
+import { useLifecycles } from 'react-use'
+
 export function Home() {
+  // react-use示例
+  useLifecycles(
+    () => {
+      console.log('Home mounted')
+    },
+    () => {
+      console.log('Home unmounted')
+    }
+  )
+
   return (
     <div className="h-50vh center text-(3xl blue-500) font-bold">
       主页 <span className="i-gis:earth-asia ml-3"></span>
